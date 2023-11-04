6648b20dba54d3dce1a5e9f62ea3c8040866ec1c
Vahdet Karatas <38797434+vahdetkaratas@users.noreply.github.com>
2023-11-04 21:41:52 +0200

    first commit

---
+++ lib/tabPages/profile_tab.dart
@@ -0,0 +1,31 @@
+import 'package:drivers_app/global/global.dart';
+import 'package:drivers_app/splashScreen/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+
+class ProfileTabPage extends StatefulWidget {
+  const ProfileTabPage({Key? key}) : super(key: key);
+
+  @override
+  _ProfileTabPageState createState() => _ProfileTabPageState();
+}
+
+
+
+class _ProfileTabPageState extends State<ProfileTabPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        child: const Text(
+          "Sign Out",
+        ),
+        onPressed: ()
+        {
+          fAuth.signOut();
+          Navigator.push(context, MaterialPageRoute(builder: (c)=> const MySplashScreen()));
+        },
+      ),
+    );
+  }
+}
